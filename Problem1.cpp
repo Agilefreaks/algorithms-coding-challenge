75c0493291c3faf02eeaf0381bcf38d680bbea3d
Tiboldi_Roberto <83965956+TiboldiRoberto@users.noreply.github.com>
2022-01-25 14:23:25 +0200

    Added more tests and changed names of variables

--- Problem1.cpp
+++ Problem1.cpp
@@ -4,19 +4,45 @@
 using namespace std;
 
 int sumEvenNumbers(int[], int);
-int vec1[5] = { 2,13,15,6,2 };
-int vec2[10] = { 2,4,17,19,22,45,17,2,99,77 };
+
+int vector1[5] = { 2,13,15,6,2 };
+int vector2[10] = { 2,4,17,19,22,45,17,2,99,77 };
+int vector3[5] = { 3,5,7,9,11};
+int vector4[5] = { 2,4,6,8,10};
+int vector5[5];
 
 TEST(Sum_Even_Number_Tests, Test1) {
-	EXPECT_EQ(sumEvenNumbers(vec1, 5), 10);
-	EXPECT_EQ(sumEvenNumbers(vec2, 10), 31);
+	EXPECT_EQ(sumEvenNumbers(vector1, 5), 10);
 }
 
-int sumEvenNumbers(int v[], int l) {
-	int s = 0;
-	for (int i = 0; i < l; i++) {
-		if (v[i] % 2 == 0)
-			s += v[i];
+TEST(Sum_Even_Number_Tests, Test2) {
+	EXPECT_EQ(sumEvenNumbers(vector2, 10), 30);
+}
+
+TEST(Sum_Even_Number_Tests, Test3) {
+	EXPECT_EQ(sumEvenNumbers(vector3, 5), 0);
+}
+
+TEST(Sum_Even_Number_Tests, Test4) {
+	EXPECT_EQ(sumEvenNumbers(vector4, 5), 30);
+}
+
+TEST(Sum_Even_Number_Tests, Test5) {
+	EXPECT_EQ(sumEvenNumbers(vector5, 5), 0);
+}
+
+TEST(Sum_Even_Number_Tests, Test6) {
+	EXPECT_EQ(sumEvenNumbers(vector5, 0), 0);
+}
+
+int sumEvenNumbers(int vector[], int length) {
+	if (length != 0) {
+		int sumOfEvenNumbers = 0;
+		for (int index = 0; index < length; index++) {
+			if (vector[index] % 2 == 0)
+				sumOfEvenNumbers += vector[index];
+		}
+		return sumOfEvenNumbers;
 	}
-	return s;
+	else return 0;
 }
