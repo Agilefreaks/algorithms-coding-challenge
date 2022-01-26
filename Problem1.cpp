6b33fc3c3dd93e3a09828874487d3e84d7d36792
Tiboldi_Roberto <83965956+TiboldiRoberto@users.noreply.github.com>
2022-01-26 11:39:43 +0200

    Update Problem1

--- Problem1.cpp
+++ Problem1.cpp
@@ -3,38 +3,6 @@
 
 using namespace std;
 
-int sumEvenNumbers(int[], int);
-
-int vector1[5] = { 2,13,15,6,2 };
-int vector2[10] = { 2,4,17,19,22,45,17,2,99,77 };
-int vector3[5] = { 3,5,7,9,11};
-int vector4[5] = { 2,4,6,8,10};
-int vector5[5];
-
-TEST(Sum_Even_Number_Tests, Test1) {
-	EXPECT_EQ(sumEvenNumbers(vector1, 5), 10);
-}
-
-TEST(Sum_Even_Number_Tests, Test2) {
-	EXPECT_EQ(sumEvenNumbers(vector2, 10), 30);
-}
-
-TEST(Sum_Even_Number_Tests, Test3) {
-	EXPECT_EQ(sumEvenNumbers(vector3, 5), 0);
-}
-
-TEST(Sum_Even_Number_Tests, Test4) {
-	EXPECT_EQ(sumEvenNumbers(vector4, 5), 30);
-}
-
-TEST(Sum_Even_Number_Tests, Test5) {
-	EXPECT_EQ(sumEvenNumbers(vector5, 5), 0);
-}
-
-TEST(Sum_Even_Number_Tests, Test6) {
-	EXPECT_EQ(sumEvenNumbers(vector5, 0), 0);
-}
-
 int sumEvenNumbers(int vector[], int length) {
 	if (length != 0) {
 		int sumOfEvenNumbers = 0;
